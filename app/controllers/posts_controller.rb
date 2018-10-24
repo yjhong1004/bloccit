c41a0845008a81b2ad2e548c7306d1444bb845d6
Jenny <yjhong1004@gmail.com>
2018-10-23 18:54:21 -0700

    change in index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @posts.each_with_index do |post, index|
-      if index % 5 == 0
+      if index % 4 == 0
         post.title = "SPAM"
       end
     end
