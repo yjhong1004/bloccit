f48c56df0f57a5ac1f74a9270083b36580f83638
Jenny <yjhong1004@gmail.com>
2018-11-13 14:54:57 -0800

    second sponsored posts changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :topics do
      resources :posts, except: [:index]
-     resources :sponsored_posts
+     resources :sponsored_posts, except: [:index]
   end
   resources :questions
 
