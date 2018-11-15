9ebc627faac40c97367287496cf68366c57f89bf
Jenny <yjhong1004@gmail.com>
2018-11-15 14:41:56 -0800

    assignment signing up first changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,17 +22,18 @@ RSpec.describe User, type: :model do
        expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
      end
 
-   describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+     describe "invalid user" do
+       let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+       let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
 
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
+       it "should be an invalid user due to blank name" do
+         expect(user_with_invalid_name).to_not be_valid
+       end
 
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
+       it "should be an invalid user due to blank email" do
+         expect(user_with_invalid_email).to_not be_valid
+       end
 
-   end
+    end
+ end
 end
