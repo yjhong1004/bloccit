9c6ff28b20347d3d3b822667e0423bc77b7cafc0
Jenny <yjhong1004@gmail.com>
2018-11-20 14:52:41 -0800

    comments authorization changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
      resources :posts, except: [:index]
      resources :sponsored_posts, except: [:index]
   end
+
+  resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
+   
   resources :questions
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_084252) do
+ActiveRecord::Schema.define(version: 2018_11_20_222031) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2018_11_20_084252) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
