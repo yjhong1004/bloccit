79005d4e8fdac666de2688854085077cd5e71e14
Jenny <yjhong1004@gmail.com>
2018-11-19 16:40:42 -0800

    assignment signing in changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,14 @@ module ApplicationHelper
   def form_group_tag(errors, &block)
        css_class = 'form-group'
        css_class << ' has-error' if errors.any?
-  # #5
        content_tag :div, capture(&block), class: css_class
-     end
-   
+  end
+
+  def avatar_url(user)
+    gravatar_id =
+    Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+
+
 end
