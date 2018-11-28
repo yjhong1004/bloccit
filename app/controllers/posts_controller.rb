d03cf28e8b78c1fa3458666f08212e46c180d0b1
Jenny <yjhong1004@gmail.com>
2018-11-27 20:04:52 -0800

    authorization assignment second changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
+  before_action :stop_moderator, only: [:new, :delete]
 
   def show
     @post = Post.find(params[:id])
@@ -65,6 +66,13 @@ class PostsController < ApplicationController
      unless current_user == post.user || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to [post.topic, post]
+     end
+   end
+   def stop_moderator
+     post = Post.find(params[:id]) 
+     if current_user.moderator?
+       flash[:alert] = "You must be the post's user or admin to do that."
+       redirect_to[post.topic, post]
      end
    end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,8 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
-
+  before_action :authorize_user, except: [:index, :show, :edit, :update]
+  before_action :stop_all_not_moderator_admin, only: [:edit, :update]
 
   def index
      @topics = Topic.all
@@ -67,5 +67,11 @@ class TopicsController < ApplicationController
        redirect_to topics_path
      end
   end
+def stop_all_not_moderator_admin
+  unless current_user.admin? || current_user.moderator?
+    flash[:alert] = "You must be an admin or moderator to do that."
+    redirect_to topics_path
+  end
+end
 
 end
