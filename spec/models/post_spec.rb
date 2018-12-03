ca7009936eb740b2b14304c5749c98dbf2447003
Jenny <yjhong1004@gmail.com>
2018-12-03 13:20:44 -0800

    checkpoint voting post changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -30,8 +30,8 @@ RSpec.describe Post, type: :model do
 
   describe "voting" do
      before do
-       3.times { post.votes.create!(value: 1) }
-       2.times { post.votes.create!(value: -1) }
+       3.times { post.votes.create!(value: 1, user: user) }
+       2.times { post.votes.create!(value: -1, user: user) }
        @up_votes = post.votes.where(value: 1).count
        @down_votes = post.votes.where(value: -1).count
      end
@@ -59,13 +59,13 @@ RSpec.describe Post, type: :model do
 
        it "updates the rank when an up vote is created" do
          old_rank = post.rank
-         post.votes.create!(value: 1)
+         post.votes.create!(value: 1, user: user)
          expect(post.rank).to eq (old_rank + 1)
        end
 
        it "updates the rank when a down vote is created" do
          old_rank = post.rank
-         post.votes.create!(value: -1)
+         post.votes.create!(value: -1, user: user)
          expect(post.rank).to eq (old_rank - 1)
        end
      end
