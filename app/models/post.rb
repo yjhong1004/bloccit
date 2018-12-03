af3847138bad92c39c437c2f9c816ea427525f56
Jenny <yjhong1004@gmail.com>
2018-12-03 13:14:10 -0800

    voting assignment changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
    belongs_to :user
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
+   after_create :create_vote
    default_scope { order('rank DESC') }
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
@@ -25,5 +26,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   private
+
+   def create_vote
+     user.votes.create(value: 1, post: self)
    end
 end
