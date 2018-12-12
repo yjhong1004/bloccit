e3824207ed8be8d7b9f554e2b84242d210d884f3
Jenny <yjhong1004@gmail.com>
2018-12-12 02:45:08 -0800

    assignment favorites first changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
    has_many :favorites, dependent: :destroy
+   after_create :create_favorite
    default_scope { order('rank DESC') }
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
@@ -26,5 +27,9 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+   def create_favorite
+     Favorite.create(post: self, user: self.user)
+     FavoriteMailer.new_post(self).deliver_now
    end
 end
