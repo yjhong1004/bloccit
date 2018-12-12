ffb4b2c4136c48fe111169c81e46116a17245748
Jenny <yjhong1004@gmail.com>
2018-12-12 02:16:50 -0800

    configuration changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
- 
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
@@ -61,5 +61,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost' }
 
-  config.raise_delivery_errors = true
+  
 end
